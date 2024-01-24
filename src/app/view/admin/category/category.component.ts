cef23ede9166516ddaf13baf93c8ab7924aa0795
EmanuelSerban <serbanemanuel071195@yahoo.com>
2024-01-24 11:10:58 +0200

    Creare clasa componenta product

--- src/app/view/admin/category/category.component.ts
+++ src/app/view/admin/category/category.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Category {
+  id: number = 0;
+  name: string = "";
+
+  Constructor(id: number, name: string){
+    this.id = id;
+    this.name = name;
+  }
+}
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
+  tableData: any[] = [
+    { id: 1, name: 'a'}
+  ];
+  newCategory: { id: number, name: string} = { id: 0, name: ""};
 
+addCategory() {
+  this.tableData.push(new Category)
+}
 }
--- src/app/view/admin/products/products.component.ts
+++ src/app/view/admin/products/products.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Product {
+
+  ref: number = 0;
+  name: string = "";
+  quantity: number = 0;
+  price: number = 0;
+  active: boolean =true;
+
+  Constructor(ref: number, name: string, quantity: number, price: number, active: boolean) {
+  this.ref = ref;
+  this.name = name;
+  this.quantity = quantity;
+  this.price = price;
+  this.active = active;
+  }
+ }
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -10,16 +26,13 @@ export class ProductsComponent {
     { ref: 1, name: 'a', quantity: 2, price: 5, active: false },
     { ref: 2, name: 'b', quantity: 50, price: 99, active: true }
   ];
-  newProduct: {ref: number, name: string, quantity: number, price: number, active: boolean}= {ref: 0, name:"", quantity: 0, price: 0, active: true};
-
-
+  newProduct: { ref: number, name: string, quantity: number, price: number, active: boolean } = { ref: 0, name: "", quantity: 0, price: 0, active: true };
 
   selectedColumn: string = '';
   inputText: string = '';
 
 
   populateTable() {
-  this.tableData.push({...this.newProduct});
+    this.tableData.push(new Product());
   }
-
 }
